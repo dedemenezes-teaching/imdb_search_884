e2fff3538a1576c149a05a7046a9650c8057d0f4
Andre Menezes <djdedemenezes@gmail.com>
2022-06-02 10:26:15 -0300

    pg serach done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,11 +17,12 @@ class MoviesController < ApplicationController
     # jump
     # jumping, jumped, jumps
     # christopher nolan
-    sql_query = "movies.title @@ :query OR
-                 movies.synopsis @@ :query OR
-                 directors.first_name @@ :query OR
-                 directors.last_name @@ :query"
-    @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%") if params[:query].present?
+    # sql_query = "movies.title @@ :query OR
+    #              movies.synopsis @@ :query OR
+    #              directors.first_name @@ :query OR
+    #              directors.last_name @@ :query"
+    # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%") if params[:query].present?
 
+    @movies = Movie.search_by_title_and_synopsis(params[:query]) if params[:query].present?
   end
 end
